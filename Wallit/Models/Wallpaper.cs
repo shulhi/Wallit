7ee2412842d4b69df3787fb85e11d500c7f8358e
Shulhi Sapli <shulhi@gmail.com>
2013-06-17 22:06:36 +0800

    added wallpaper model

---
+++ Wallit/Models/Wallpaper.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallit.Models
+{
+    public class Wallpaper
+    {
+        [JsonProperty("url")]
+        public string Uri { get; set; }
+        public string Title { get; set; }
+    }
+}
