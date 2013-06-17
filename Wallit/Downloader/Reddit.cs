46c841e9bc52599410bdec61d2cb2cd5176e2751
Shulhi Sapli <shulhi@gmail.com>
2013-06-17 22:02:06 +0800

    added reddit r/wallpaper downloader

---
+++ Wallit/Downloader/Reddit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallit.Downloader
+{
+    public class Reddit
+    {
+        private HttpClient _client;
+
+        public Reddit()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<string> DownloadJson(string uri)
+        {
+            return await _client.GetStringAsync(uri);
+        }
+    }
+}
--- Wallit/Program.cs
+++ Wallit/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wallit.Downloader;
 
 namespace Wallit
 {
@@ -12,6 +13,8 @@ namespace Wallit
     {
         static void Main(string[] args)
         {
+            var downloader = new ImageDownloader();
+            downloader.DownloadImage("http://i.imgur.com/Y0qNmtp.jpg", @"F:\reddit.jpg");
 
             Console.ReadLine();
         }
